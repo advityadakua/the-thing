cf7f2d31064a670fe20639544595490d580e4453
Advitya D. <advitya.dakua@student.tdsb.on.ca>
2025-10-22 20:29:46 +0000

    Pre-Recovery checkpoint

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python311Full
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.wheel
+    pkgs.python311Packages.pillow
+    pkgs.python311Packages.reportlab
+  ];
+}
